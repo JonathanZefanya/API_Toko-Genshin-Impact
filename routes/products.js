5888b7df3087d05a629306088468dc9584eddc75
JonathanZefanya <jonathan.zefanya16@gmail.com>
2025-01-03 23:34:37 +0700

    fix: correct variable reference in filtering logic of GET products route

--- routes/products.js
+++ routes/products.js
@@ -26,7 +26,7 @@ router.get("/", (req, res) => {
   // Filter berdasarkan categoryId
   if (categoryId) {
     filteredProducts = filteredProducts.filter(
-      (prodak) => prodak.category.id === category.id
+      (prodak) => prodak.category.id === categoryId
     );
   }
 
