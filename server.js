28c5f2da22f4d1de30e76b4b540b11269de0b579
JonathanZefanya <jonathan.zefanya16@gmail.com>
2025-01-03 16:10:40 +0700

    refactor server.js to export app for Vercel deployment

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+
+// const productsRoute = require("./routes/products");
+// const bannersRoute = require("./routes/banners");
+// const categoriesRoute = require("./routes/categories");
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// // Routes
+// app.use("/products", productsRoute);
+// app.use("/banners", bannersRoute);
+// app.use("/categories", categoriesRoute);
+
+// // Server
+// const PORT = 9999;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -17,8 +42,5 @@ app.use("/products", productsRoute);
 app.use("/banners", bannersRoute);
 app.use("/categories", categoriesRoute);
 
-// Server
-const PORT = 9999;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// Ekspor aplikasi untuk Vercel
+module.exports = app;
